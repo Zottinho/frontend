7aab09118a296dbf903eee333d86e409034aad9e
Zottinho <caiocesar_andrade@hotmail.com>
2025-10-25 11:28:23 -0300

    modified:   Projeto1/cadastro.html 	new file:   Projeto1/css/styles.css 	modified:   Projeto1/index.html 	new file:   Projeto1/js/script.js 	modified:   Projeto1/projeto.html

---
+++ Projeto1/js/script.js
@@ -0,0 +1,45 @@
+/*Mobile */
+function toggleMenu() {
+    const navMenu = document.getElementById('navMenu');
+    navMenu.classList.toggle('active');
+}
+
+/*Scroll leve */
+function scrollActive(sectionId) {
+    const section = document.getElementById('sectionId');
+    if (!section) return;
+
+    const headerHeight = 70;
+    const sectionPosition = section.offsetTop - headerHeight;
+
+    window.scrollTo({
+        top: sectionPosition,
+        behavior: 'smooth'
+    });   
+
+    const menu = document.getElementById('navMenu');
+    menu.classList.remove('active');
+}    
+
+/*Cadastro */
+
+function handleSubmit(event) {
+    event.preventDefault();
+    
+    const form = document.getElementById('volunteerForm');
+    const formData = {
+        nome: form.nome.value,
+        email: form.email.value,
+        telefone: form.telefone.value,
+        idade: form.idade.value,
+        disponibilidade: form.disponibilidade.value,
+        areainteresse: form.areainteres.value,
+        experiencia: form.experiencia.value,
+        motivacao: form.motivacao.value,
+        dataCadastro: new Date().toLocaleDateString()
+    
+    }
+
+let voluntarios = JSON.parse(localStorage.getItem('voluntarios')) || [];
+    voluntarios.push(formData);
+    localStorage.setItem('voluntarios', JSON.stringify(voluntarios));}
